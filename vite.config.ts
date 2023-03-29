e4fbaf26541a8b27d35d35d04702957e70c98301
Molly Kan <mollykannn@gmail.com>
2023-03-29 22:57:54 +0800

    fixed build problem

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import StylelintPlugin from 'vite-plugin-stylelint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'esnext'
+  },
   plugins: [
     vue(),
     VitePWA({
