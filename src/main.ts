5a81e6aa8e83bc1b8356b0351543db35175db641
Molly Kan <mollykannn@gmail.com>
2023-12-17 21:45:45 +0800

    update package & test electron

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,11 @@ import '@/assets/css/style.scss'
 import initSqlJs from 'sql.js'
 
 const app = createApp(App)
-app.config.globalProperties.SQL = await initSqlJs();
-app.mount('#app')
+app.config.globalProperties.SQL = await initSqlJs({
+  // locateFile: file => `https://sql.js.org/dist/${file}`
+  locateFile: file => `./${file}`
+});
+app.mount('#app').$nextTick(() => {
+  postMessage({ payload: 'removeLoading' }, '*')
+})
+
--- vite.config.ts
+++ vite.config.ts
@@ -4,67 +4,82 @@ import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 import eslintPlugin from 'vite-plugin-eslint'
 import StylelintPlugin from 'vite-plugin-stylelint'
+import electron from 'vite-plugin-electron/simple'
+import path from 'node:path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/kobo-book-exporter/',
-  build: {
-    target: 'esnext'
-  },
-  plugins: [
-    vue(),
-    VitePWA({
-      workbox: {
-        sourcemap: true
-      },
-      mode: 'development',
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
-      manifest: {
-        name: 'Kobo Book Exporter',
-        short_name: 'Kobo Book Exporter',
-        description: 'Kobo Book Exporter',
-        theme_color: '#0079d2',
-        start_url: './index.html',
-        lang: 'zh-Hant-HK',
-        dir: 'ltr',
-        orientation: 'portrait',
-        icons: [
-          {
-            src: 'img/icons/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'img/icons/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: 'img/icons/android-chrome-maskable-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'maskable'
-          },
-          {
-            src: 'img/icons/android-chrome-maskable-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable'
-          }
-        ]
-      }
-    }),
-    StylelintPlugin({
-      fix: true,
-    }),
-    eslintPlugin({
-      include: ['src/**/*.vue', 'src/**/*.js'],
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const isElectron = mode === 'electron'
+  return {
+    ...(!isElectron && {base: '/kobo-book-exporter/'}),
+    build: {
+      ...(!isElectron && { outDir: 'dist-page', emptyOutDir: true }),
+      target: 'esnext'
     },
-  },
+    plugins: [
+      vue(),
+      electron({
+        main: {
+          entry: 'electron/main.ts',
+        },
+        preload: {
+          input: path.join(__dirname, 'electron/preload.ts'),
+        },
+        renderer: {},
+      }),
+      VitePWA({
+        workbox: {
+          sourcemap: true
+        },
+        mode: 'development',
+        registerType: 'autoUpdate',
+        includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+        manifest: {
+          name: 'Kobo Book Exporter',
+          short_name: 'Kobo Book Exporter',
+          description: 'Kobo Book Exporter',
+          theme_color: '#0079d2',
+          start_url: './index.html',
+          lang: 'zh-Hant-HK',
+          dir: 'ltr',
+          orientation: 'portrait',
+          icons: [
+            {
+              src: 'img/icons/android-chrome-192x192.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: 'img/icons/android-chrome-512x512.png',
+              sizes: '512x512',
+              type: 'image/png'
+            },
+            {
+              src: 'img/icons/android-chrome-maskable-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+              purpose: 'maskable'
+            },
+            {
+              src: 'img/icons/android-chrome-maskable-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'maskable'
+            }
+          ]
+        }
+      }),
+      StylelintPlugin({
+        fix: true,
+      }),
+      eslintPlugin({
+        include: ['src/**/*.vue', 'src/**/*.js'],
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  }
 })
